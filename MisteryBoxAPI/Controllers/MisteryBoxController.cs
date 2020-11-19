6a7bc17fb2b58009808fd0833c6177473a8e2712
Benjamín García <benjahergar@gmail.com>
2020-11-19 22:05:50 +0100

    three random list elements with  zip

--- MisteryBoxAPI/Controllers/MisteryBoxController.cs
+++ MisteryBoxAPI/Controllers/MisteryBoxController.cs
@@ -5,6 +5,7 @@ using MisteryBoxDomain.Entities;
 using MisteryBoxDomain.Services;
 using System.Linq;
 using System;
+using MisteryBoxAPI.Crosscutting;
 
 namespace MisteryBoxAPI.Controllers
 {
@@ -30,12 +31,10 @@ namespace MisteryBoxAPI.Controllers
             var dataRopa = _ropaService.GetAll();
             var dataTecn = _tecnologiaService.GetAll();
             await Task.WhenAll(dataCocina, dataRopa, dataTecn);
-            List<Item> lista = new List<Item>();
-
-            lista.AddRange(dataCocina.Result);
-            lista.AddRange(dataRopa.Result);
-            lista.AddRange(dataTecn.Result);
-            return Ok(lista.OrderBy(x => Guid.NewGuid()).ToList());
+            return Ok(dataCocina.Result.OrderBy(x => Guid.NewGuid())
+                .ZipThree(dataRopa.Result.OrderBy(x => Guid.NewGuid())
+                , dataTecn.Result.OrderBy(x => Guid.NewGuid())
+                , (itemCocina, itemRopa, itemTech) => new { itemCocina, itemRopa, itemTech }));
         }
 
         public async Task<IHttpActionResult> ObtenerCajaMisteriosaV1()
